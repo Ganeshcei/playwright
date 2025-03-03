57164c538ceb393eb0de09d0b14b484af4745e35
Ganeshcei <dganesh@ceiamerica.com>
2025-03-03 12:00:47 +0530

    Added basic login functionality in login.py

---
+++ test.py
@@ -0,0 +1,8 @@
+# Open the input file in read mode and the output file in write mode
+with open('AWS.txt', 'r') as input_file, open('result.txt', 'w') as output_file:
+    # Iterate through each line in the input file
+    for line in input_file:
+        # Check if 'T605' is not in the current line
+        if 'T605' not in line:
+            # Write the line to the output file
+            output_file.write(line)
